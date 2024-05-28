485e368183776b545b1da4b6928363651da165d8
Dieter-Lessmann <137188303+Dieter-Lessmann@users.noreply.github.com>
2024-05-28 10:39:57 +0200

    Update height_profile_script.js

--- js/height_profile_script.js
+++ js/height_profile_script.js
@@ -1,98 +1,85 @@
 // Funktion zum Laden der Koordinaten aus der track_egge_2.js Datei
 function loadCoordinates() {
-    // Annahme: Die Funktion loadCoordinates() ist in track_egge_2.js definiert und gibt die Koordinaten zurück
-    // Hier wird ein Beispiel verwendet, um die Funktionalität zu simulieren
-    return track_egge_2.features[0].geometry.coordinates[0];
+    return track_egge_2.features[0].geometry.coordinates;
 }
 
 // Deklaration der Variable außerhalb des Ereignishandlers
-var coordinates;
+var coordinates = loadCoordinates(); // Initialisierung der Koordinaten
 
 // Funktion zur Erstellung des Höhenprofils
 function createHeightProfile(coordinates, cumulativeDistances, canvas) {
-    console.log("Coordinates:", coordinates);
-    console.log("Cumulative Distances:", cumulativeDistances);
     var heights = extractHeights(coordinates);
-    console.log("Heights:", heights);
 
     // Überprüfe, ob das Diagramm bereits existiert, bevor es zerstört wird
     var existingChart = Chart.getChart(canvas);
-    console.log("Existing Chart:", existingChart);
     if (existingChart) {
         existingChart.destroy();
-        console.log("Existing Chart Destroyed");
     }
 
-    try {
-        var ctx = canvas.getContext('2d');
-        console.log("Context:", ctx);
-        var chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: cumulativeDistances, // Verwende kumulative Distanzen als X-Achsen-Werte
-                datasets: [{
-                    label: 'Höhenprofil',
-                    data: heights,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        title: {
-                            display: true,
-                            text: 'Höhe (m)'
-                        }
+    var ctx = canvas.getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: cumulativeDistances,
+            datasets: [{
+                label: 'Höhenprofil',
+                data: heights,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Höhe (m)'
+                    }
+                },
+                x: {
+                    type: 'linear',
+                    title: {
+                        display: true,
+                        text: 'Distanz (km)'
                     },
-                    x: {
-                        type: 'linear', // Typ der Achse auf linear setzen
-                        title: {
-                            display: true,
-                            text: 'Distanz (km)'
+                    ticks: {
+                        callback: function(value) {
+                            return value.toFixed(1);
+                        }
+                    }
+                }
+            },
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        title: function(tooltipItems) {
+                            var index = tooltipItems[0].dataIndex;
+                            var distance = cumulativeDistances[index];
+                            return 'Distanz: ' + distance.toFixed(2) + ' km';
                         },
-                        ticks: {
-                            callback: function(value, index, values) {
-                                return value.toFixed(1); // Skalierte Distanzwerte mit 2 Nachkommastellen anzeigen
-                            }
+                        label: function(tooltipItem) {
+                            var height = tooltipItem.raw;
+                            return 'Höhe: ' + height.toFixed(0) + ' m';
                         }
                     }
-                },
-                plugins: {
-                    tooltip: {
-                        callbacks: {
-                            title: function(tooltipItems) {
-                                var index = tooltipItems[0].dataIndex;
-                                var distance = cumulativeDistances[index];
-                                return 'Distanz: ' + distance.toFixed(2) + ' km';
-                            },
-                            label: function(tooltipItem) {
-                                var height = tooltipItem.raw;
-                                return 'Höhe: ' + height.toFixed(0) + ' m';
-                            }
-                        }
-                    }
-                },
-                onHover: function(evt, elements) {
-                    if (elements.length > 0) {
-                        const index = elements[0].index;
-                        const latLng = getLatLngFromChartX(index);
-                        if (latLng) {
-                            try {
-                                window.opener.updateMarker(latLng[0], latLng[1]);
-                            } catch (error) {
-                                console.error("Error calling updateMarker:", error);
-                            }
+                }
+            },
+            onHover: function(evt, elements) {
+                if (elements.length > 0) {
+                    const index = elements[0].index;
+                    const latLng = getLatLngFromChartX(index);
+                    if (latLng) {
+                        try {
+                            window.opener.updateMarker(latLng[0], latLng[1]);
+                        } catch (error) {
+                            console.error("Error calling updateMarker:", error);
                         }
                     }
                 }
             }
-        });
-        console.log("Chart created:", chart);
-    } catch (error) {
-        console.error("Error creating chart:", error);
-    }
+        }
+    });
 }
 
 // Funktion zur Berechnung der kumulativen Distanzen zwischen den Punkten
@@ -104,7 +91,6 @@ function calculateCumulativeDistances(coordinates) {
         totalDistance += distance;
         cumulativeDistances.push(totalDistance);
     }
-    console.log("Cumulative Distances Calculated:", cumulativeDistances);
     return cumulativeDistances;
 }
 
@@ -124,26 +110,18 @@ function calculateDistance(coord1, coord2) {
             Math.sin(dlon / 2) * Math.sin(dlon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    var distance = R * c; // Entfernung in Kilometern
+    var distance = R * c;
     return distance;
 }
 
 // Funktion zur Extrahierung der Höhendaten aus den Koordinaten
 function extractHeights(coordinates) {
-    // Extrahiere die Höhendaten (drittes Element) aus jedem Koordinatenarray
     const heights = coordinates.map(coord => coord[2]);
-    console.log("Extracted Heights:", heights);
     return heights;
-}
-
-// Funktion zur Konvertierung von Grad in Radiant
-function deg2rad(deg) {
-    return deg * (Math.PI / 180);
 }
 
 // Funktion zum Abrufen der Lat/Lng aus der X-Koordinate des Diagramms
 function getLatLngFromChartX(chartX) {
-    // Index des nächstgelegenen Punkts im Track finden
     const index = Math.round(chartX);
     if (index >= 0 && index < coordinates.length) {
         const lat = coordinates[index][1];
@@ -152,3 +130,29 @@ function getLatLngFromChartX(chartX) {
     }
     return null;
 }
+
+// Funktion zum Konvertieren von Grad in Radiant
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+// Funktion zum Aktualisieren der Markerposition
+function updateMarker(lat, lng) {
+    if (window.opener) {
+        window.opener.updateMarker(lat, lng);
+    } else {
+        console.error("window.opener is not defined");
+    }
+}
+
+// Initialisierung des Diagramms nach dem Laden der Seite
+window.onload = function() {
+    var canvas = document.getElementById('heightProfile');
+    if (canvas) {
+        var coordinates = loadCoordinates();
+        var cumulativeDistances = calculateCumulativeDistances(coordinates);
+        createHeightProfile(coordinates, cumulativeDistances, canvas);
+    } else {
+        console.error("Canvas element not found.");
+    }
+};
